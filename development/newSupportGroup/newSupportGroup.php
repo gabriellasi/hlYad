218ade783ed816516e6aab8c1c0611391b1acf27
meitalmishni <37497195+meitalmishni@users.noreply.github.com>
2018-05-13 14:16:33 +0300

    new support group
    update new support group modal

--- development/newSupportGroup/newSupportGroup.php
+++ development/newSupportGroup/newSupportGroup.php
@@ -14,7 +14,9 @@ if ($conn->connect_error) {
 
 mysqli_set_charset($conn,"utf8");
 
-    
+$errors; // hold validation errors
+$data = array(); //  pass back data
+
 $supportGroupName=$_POST["supportGroupName"];
 $supportGroupManager=$_POST["supportGroupManager"];
 $riskType=$_POST["riskType"];
@@ -23,12 +25,32 @@ $email=$_POST["email"];
 $city=$_POST["cityName"];
 $street=$_POST["street"];
 $address= $street . ' ' . $city;
+$minAge=$_POST["min_age"];
+$maxAge=$_POST["max_age"];
+$url=$_POST["url"];
+
+$checkExistVal="SELECT ID FROM support_group where name='$supportGroupName' and address='$address' and risk='$riskType'";
+$result = $conn->query($checkExistVal);
 
 
-
-$InsertSupportGroup="INSERT INTO support_group (name,risk,manager,phone,address,mail) VALUES('".$supportGroupName."','".$riskType."','".$supportGroupManager."','".$phone."','".$address."','".$email."');";
+if($result->num_rows==0){  
+$InsertSupportGroup="INSERT INTO support_group (name,risk,manager,phone,address,mail,min_age,max_age,website_url) VALUES('".$supportGroupName."','".$riskType."','".$supportGroupManager."','".$phone."','".$address."','".$email."','".$minAge."','".$maxAge."','".$url."');";
 $conn->query($InsertSupportGroup);
+}
+else{
+    $errors ='קבוצת תמיכה קיימת במערכת';
+}
 
+if(!empty($errors)){
+    $data['success']=false;
+    $data['message']=$errors;
+}
+else{
+    $data['success']=true;
+    $data['message']='קבוצת התמיכה הוספה למערכת בהצלחה';
+}
+
+echo $data['message'];
 
 $conn->close();
 ?>
