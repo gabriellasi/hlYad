8cefaa34efb6d96c078ee2984305ef046e7fa3a3
barnachum <barnachum36@gmail.com>
2018-05-15 11:39:50 +0300

    Latest update for system file - all shelter and support group score by distance and age!!!

--- development/system/system (2).php
+++ development/system/system (2).php
@@ -42,15 +42,20 @@ $addressNamesGroup= array();
 while($rowShelter=mysqli_fetch_assoc($shelterDestination_res)){
     $shelterArr[]=$rowShelter['address'];
 }
+echo 'Address of shelters withoud distance'.'<br>';
+print_r($shelterArr);
 
 //creating array of support groups addresses
 while($rowGroup=mysqli_fetch_assoc($groupDestination_res)){
     $groupArr[]=$rowGroup['address'];
 }
 
+echo 'Address of groups withoud distance'.'<br>';
+print_r($groupArr);
+
 //this is distance array of support group
 foreach($groupArr as $valueGroup){
-    echo $valueGroup.'<br>';
+    //echo $valueGroup.'<br>';
     
     $urlContentsGroup = "https://maps.googleapis.com/maps/api/distancematrix/json?units=metric&origins=".urlencode($cityOrigin)."&destinations=".urlencode("$valueGroup")."&key=AIzaSyAgnrRznulN8DCt5jeMMrQf75HwV0f6thM";
     $group_data = file_get_contents($urlContentsGroup);
@@ -61,19 +66,22 @@ foreach($groupArr as $valueGroup){
     $addressNamesGroup[]=$valueGroup;
     
     $destinationKM_group=substr($destinationKM_group,0,-2);
-    $destinationKM_group=intval($destinationKM_group);
-    echo $destinationKM_group.'<br>';
+    $destinationKM_group=doubleval($destinationKM_group);
+    //echo $destinationKM_group.'<br>';
     $distanceResGroup[]=$destinationKM_group;
 }
 
-$addresDistGroup= array_combine($distanceResGroup,$addressNamesGroup);
-ksort($addresDistGroup);
-print_r ($addresDistGroup);
+$addressDistGroup= array_combine($distanceResGroup,$addressNamesGroup);
+ksort($addressDistGroup);
+
+echo "this is the combined 100 percent and support addresses after sort"."<br>";
+print_r ($addressDistGroup);
 
 
 
 
 //this is distance array of shelter
+echo "this is the address for each shelter"."<br>";
 foreach($shelterArr as $valueShelter){
     echo $valueShelter . "<br>";
     $urlContentsShelter = "https://maps.googleapis.com/maps/api/distancematrix/json?units=metric&origins=".urlencode($cityOrigin)."&destinations=".urlencode("$valueShelter")."&key=AIzaSyAgnrRznulN8DCt5jeMMrQf75HwV0f6thM";
@@ -85,17 +93,27 @@ foreach($shelterArr as $valueShelter){
    $addressNamesShelter[] = $valueShelter;
    
    $destinationKM=substr($destinationKM, 0, -2);
-   $destinationKM=intval($destinationKM);
-   echo $destinationKM.'<br>';
+   $destinationKM=doubleval($destinationKM);
+   //echo $destinationKM.'<br>';
    $distanceResShelter[]= $destinationKM;
+   
 }
 
-//print_r ($distanceResShelter);
-//print_r ($addressNamesShelter);
+echo "array of shelter distance without address" .'<br>';
+print_r ($distanceResShelter);
+echo "address name shelter array".'<br>';
+print_r ($addressNamesShelter);
 
+echo "array shelter combined: adrress + distance"."<br>";
 $addressDistShelter= array_combine($distanceResShelter,$addressNamesShelter);
+//$addressDistShelter= array_combine($addressNamesShelter,$distanceResShelter);
+
+
+//echo "this is the combined array not sorted";
 //print_r ($addressDistShelter);
 ksort($addressDistShelter);
+
+echo "This is sorted combine array of distance + address - shelters" .'<br>';
 print_r ($addressDistShelter);
 
 
@@ -106,29 +124,32 @@ $percentGroup=array();
 foreach($addressNamesShelter as $addressNamesShelterValue ){
     $percentShelter[]=100;
 }
-print_r($percentShelter);
+//echo "this is the 100 percent array - shelter"."<br>";
+//print_r($percentShelter);
 
 // percent array for support group
 foreach($addressNamesGroup as $addressNamesGroupValue ){
     $percentGroup[]=100;
 }
-
+echo "this is the 100 percent array - group"."<br>";
 print_r($percentGroup);
 
 
 $percentShelterCombine=array_combine($addressNamesShelter,$percentShelter);
-$percentSupportGCombine=array_combine($addressNamesGroup,$percentGroup);
+$percentSupportCombine=array_combine($addressNamesGroup,$percentGroup);
 
-print_r($percentShelterCombine);
-print_r($percentSupportGCombine);
+//echo "this is the combined 100 percent and address - shelter"."<br>";
+//print_r($percentShelterCombine);
+echo "this is the combined 100 percent and address - support"."<br>";
+print_r($percentSupportCombine);
 
 //grading the $percentShelterCombine array according to distance
-while($valueDist = current($addressDistShelter)){
+while($address = current($addressDistShelter)){
     
-    echo $valueDist."<br>";
+    echo $address."<br>";
     $distanceKey = key ($addressDistShelter);
     echo $distanceKey."<br>";
-    $score=$percentShelterCombine[$valueDist];
+    $score=$percentShelterCombine[$address];
     
     if($distanceKey>="0" && $distanceKey <="5"){
         echo $score;
@@ -145,14 +166,15 @@ while($valueDist = current($addressDistShelter)){
         $score = $score-15;
         echo $score;
     }
-    $percentShelterCombine[$valueDist]= $score;
+    $percentShelterCombine[$address]= $score;
     next($addressDistShelter);
 }
 
 print_r($percentShelterCombine);
 
 //grading $percentShelterCombine array according to age
-while($valueDist = current($percentShelterCombine)){
+echo "this is the min and max age of every shelter"."<br>";
+while($grade = current($percentShelterCombine)){
     
     $addressKey = key ($percentShelterCombine);
     $age_range ="SELECT min_age, max_age FROM shelter where address='$addressKey'";
@@ -177,60 +199,70 @@ while($valueDist = current($percentShelterCombine)){
     next($percentShelterCombine);
 }
 
+echo "final grades after age and distance check"."<br>";
 print_r($percentShelterCombine);
 
 
-/*while($rowShelter=mysqli_fetch_assoc($shelterDestination_res)){
-    $shelterArr[]=$rowShelter['address'].'|';
-}*/
-
-/*while($rowGroup=mysqli_fetch_assoc($groupDestination_res)){
-    $groupArr[]=$rowGroup['address'].'|';
-}*/
-
-
-/*foreach($shelterArr as $valueShelter){
-    echo $valueShelter . "<br>";
+//grading the $percentSupportCombine array according to distance
+echo "address of groups"."<br>";
+while($addressGroup = current($addressDistGroup)){
+   
+    echo $addressGroup."<br>";
+    $distanceKeyGroup = key ($addressDistGroup);
+    echo $distanceKeyGroup."<br>";
+    $scoreGruop=$percentSupportCombine[$addressGroup];
+    
+    if($distanceKeyGroup>="0" && $distanceKeyGroup <="5"){
+        echo $scoreGruop;
+    }
+    elseif($distanceKeyGroup<="10"){
+        $scoreGruop = $scoreGruop-5;
+        echo $scoreGruop;
+    }
+    elseif($distanceKeyGroup<="20"){
+        $scoreGruop = $scoreGruop-10;
+        echo $scoreGruop;
+    }
+    else{
+        $scoreGruop = $scoreGruop-15;
+        echo $scoreGruop;
+    }
+    $percentSupportCombine[$addressGroup]= $scoreGruop;
+    next($addressDistGroup);
 }
 
+echo "precent support group combine array"."<br>";
+print_r($percentSupportCombine);
 
-foreach($groupArr as $valueGroup){
-    echo $valueGroup . "<br>";
-}*/
+//grading $percentSupportCombine array according to age
+echo "this is the min and max age of every support group"."<br>";
+while($gradeSupport = current($percentSupportCombine)){
+    
+    $addressKeyGroup = key ($percentSupportCombine);
+    $age_range_group ="SELECT min_age, max_age FROM support_group where address='$addressKeyGroup'";
+    $resultGroup = $conn->query($age_range_group);
+    
+    
+    if($resultGroup->num_rows > 0){
+        while($row = $resultGroup->fetch_assoc()){
+            $min_age_group = $row['min_age'];
+            $max_age_group = $row['max_age'];
+        }
+    }
+    echo "min: ".$min_age_group."<br>";
+    echo "max: ".$max_age_group."<br>";
+    $scoreGroup=$percentSupportCombine[$addressKeyGroup];
+    
+    if($userAge <  $min_age_group ||  $max_age_group<$userAge){
+        $scoreGroup =$scoreGroup-10;
+    }
+    
+    $percentSupportCombine[$addressKeyGroup]= $scoreGroup;
+    next($percentSupportCombine);
+}
 
-
-/*$destinationsShelter = implode($shelterArr);
-echo $destinationsShelter;
-
-$cityOrigin=$_GET["cityName"];
-echo $cityOrigin . "<br>";
-
-$natanya="נתניה";
-
-
-
-$urlContentsShelter = "https://maps.googleapis.com/maps/api/distancematrix/json?units=metric&origins=".urlencode($cityOrigin)."&destinations=".urlencode($shelterArr)."&key=AIzaSyAgnrRznulN8DCt5jeMMrQf75HwV0f6thM";
-
-$shelter_data = file_get_contents($urlContentsShelter);
-$distancesShelter = json_decode($shelter_data, true);
-print_r($distancesShelter);
-
-
-$distance="the distance from".$cityOrigin."to".$shelterArr[15]."is".$distancesShelter['rows'][0]['elements'][15]['distance']['text'];
-echo $distance;
-
-
-/*$distance="the distance from".$cityOrigin."to".$shelterArr[15]."is".$distancesShelter['rows'][0]['elements'][15]['distance']['text'];
-echo $distance;*/
-
-/*$urlContentsGroup = "https://maps.googleapis.com/maps/api/distancematrix/json?units=metric&origins=".urlencode($_GET[''])."&destinations=".urlencode($rowGroup)."&key=AIzaSyAgnrRznulN8DCt5jeMMrQf75HwV0f6thM";
-*/
-
-
-//free results
-/*mysqli_free_result($rowShelter);
-mysqli_free_result($rowGroup);*/
-
+echo "final grades after age and distance check - Support Group"."<br>";
+print_r($percentSupportCombine);
 
 
 
