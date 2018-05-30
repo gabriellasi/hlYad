7d20a1c07fb2f40655cb027a07da9a1434bbec41
gabriellasi <gabriellasi@mta.ac.il>
2018-05-30 23:24:12 +0300

    new Shelter update

--- development/newShelter/shelter.php
+++ development/newShelter/shelter.php
@@ -1,3 +1,20 @@
+<?php
+session_start();
+
+if (isset($_SESSION["user"])){
+    $user = $_SESSION["user"];
+    $permit = $_SESSION["permit"];
+}
+else{
+    $user = "";
+    $permit = 0;
+}
+
+//echo $user. "<br>";
+//echo $permit. "<br>";
+
+?>
+
 <!DOCTYPE html>
 <html lang="en">
     <head>
@@ -12,36 +29,173 @@
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link href="https://fonts.googleapis.com/css?family=Amatic SC" rel="stylesheet">
+            <script src="https://apis.google.com/js/platform.js" async defer></script>
+        <meta name="google-signin-scope" content="profile email">
+      <meta name="google-signin-client_id" content="230287754090-959u5p3brfb0l6tv1vd4gdl7hlmcr1t9.apps.googleusercontent.com">
+            <script>
+        var permission = <?php echo $permit?>;
+        </script>
     </head>
     
     <body dir="rtl">
-        <div class="header">
+        <div class="container">
+            <div class="row">
+                <div class="col-sm-12">
+                    <div class="header">
             <div id="log">
-                <i id="logIn"  class="fa" title="התחבר" >&#xf007;</i>
+                <a id="logToSystem" href="http://gabriellasi.mtacloud.co.il/registrationLogin/registraion_latest.php"><i id="logIn"  class="fa" title="התחבר" >&#xf007;</i></a>
             </div>
             
             <div class="text-center">
-                <a href="#"><img id="logo" src="logo.png"></a>
+                <a href="http://gabriellasi.mtacloud.co.il/home%20page/homePage.php"><img id="logo" src="logo.png"></a>
             </div>
             
-            <div class="pointer">
+            <a href="http://gabriellasi.mtacloud.co.il/newshelter/shelter.php" onclick="signOut();" id="signOut" style="display:none;"> התנתק</a>
+            <script>
+                function signOut() {
+                    gapi.auth2.init();
+                    var auth2 = gapi.auth2.getAuthInstance();
+                    auth2.signOut().then(function () {
+                        console.log('User signed out.');
+                    });
+                    <?php
+                    session_unset($_SESSION["user"]);
+                    session_unset($_SESSION["permit"]);
+                    //$_SESSION["user"] = "";
+                    //$_SESSION["permit"]= 0;
+                    session_destroy();
+                    ?>
+                    window.location.reload();
+                    refreshPage();
+                }
+            </script>
+            
+            
+            <div id="wrapper">
+                <div class="overlay"></div>
+                <!-- Sidebar -->
+                <nav class="navbar navbar-inverse navbar-fixed-top" id="sidebar-wrapper" role="navigation">
+                    <ul class="nav sidebar-nav">
+                        <li class="sidebar-brand">
+                            <a name="hlyTitle" style="font-family: 'Times New Roman'">הושט לי יד</a>
+                        </li>
+                        <hr>
+                        <li>
+                            <a href="http://gabriellasi.mtacloud.co.il/home%20page/homePage.php" style="font-family: 'Times New Roman'">דף הבית</a>
+                        </li>
+                        <li>
+                            <a href="http://gabriellasi.mtacloud.co.il/about%20-%20try/about.php" style="font-family: 'Times New Roman'">אודות</a>
+                        </li>
+                        <li>
+                            <a href="http://gabriellasi.mtacloud.co.il/activities%20page/activities.php" style="font-family: 'Times New Roman'">תחומי פעילות</a>
+                        </li>
+                        <li>
+                            <a href="http://gabriellasi.mtacloud.co.il/joinUs/joinus.php" style="font-family: 'Times New Roman'">הצטרפות כגוף מטפל</a>
+                        </li>
+                    </ul>
+                </nav>
+                <!-- /#sidebar-wrapper -->
+                
+                <!-- Page Content -->
+                <div id="page-content-wrapper">
+                    <button type="button" class="hamburger is-closed" data-toggle="offcanvas">
+                        <span class="hamb-top"></span>
+                        <span class="hamb-middle"></span>
+                        <span class="hamb-bottom"></span>
+                        </button>
+                </div>
+            </div>
+            
+            <div class="pointer" id="careService" style="display:none;">
                 <div calss="navbar">
                     <nav class="stroke">
                         <ul>
-                          <li><a href="#">דף הבית</a></li>
-                          <li><a href="#">אודות</a></li>
-                          <li><a href="#">תחומי פעילות</a></li>
-                          <li><a href="#">התנדבות</a></li>
-                          <li><a href="#">צור קשר</a></li>
+                          <li><a href="http://gabriellasi.mtacloud.co.il/newshelter/shelter.php">הוסף בית מחסה</a></li>
+                          <li><a href="http://gabriellasi.mtacloud.co.il/new%20support%20group/supportGroup.php">הוסף קבוצת תמיכה</a></li>
+                          <li><a href="http://gabriellasi.mtacloud.co.il/newLine/hotline.php">הוסף קו חם</a></li>
+                          <li><a href="http://gabriellasi.mtacloud.co.il/contact%20us/contactPage.php">יצירת קשר</a></li>
                         </ul>
                     </nav>
                 </div>
             </div>
-
+            <div class="pointer" id="adminNav" style="display:none;">
+                <div calss="navbar">
+                    <nav class="stroke">
+                        <ul>
+                          <li><a href="http://gabriellasi.mtacloud.co.il/newCategory/category.php">הוספת קטגוריה</a></li>
+                        </ul>
+                    </nav>
+                </div>
+            </div>
+            <div class="pointer" id="volunteerNav" style="display:none;">
+                <div calss="navbar">
+                    <nav class="stroke">
+                        <ul>
+                          <li><a href="http://gabriellasi.mtacloud.co.il/new%20volunteer/volunteer.php">הצטרף כמתנדב/ת</a></li>
+                          <li><a href="http://gabriellasi.mtacloud.co.il/contact%20us/contactPage.php">יצירת קשר</a></li>
+                        </ul>
+                    </nav>
+                </div>
+            </div>
+            <div class="pointer" id="youthNav">
+                <div calss="navbar">
+                    <nav class="stroke">
+                        <ul>
+                          <li><a href="http://gabriellasi.mtacloud.co.il/systemTryDesign/system.html">התאמת גוף מטפל</a></li>
+                          <li><a href="http://gabriellasi.mtacloud.co.il/Shelters%20list/shelterList.php">בתי מחסה</a></li>
+                          <li><a href="http://gabriellasi.mtacloud.co.il/support%20group%20list/supportTable.php">קבוצות תמיכה</a></li>
+                          <li><a href="http://gabriellasi.mtacloud.co.il/hot%20line%20list/hotLineTable.php">קווים חמים</a></li>
+                          <li><a href="http://gabriellasi.mtacloud.co.il/PersonalErea/personalPage_Trial.php" id="personalAreaLink" style="display:none;">איזור אישי</a></li>
+                          <li><a href="http://gabriellasi.mtacloud.co.il/contact%20us/contactPage.php">יצירת קשר</a></li>
+                        </ul>
+                    </nav>
+                </div>
+            </div>
+            
         </div>
-        <hr>
+                </div>
+            </div>
+        </div>
         
+                <script>
+                if (permission !=0){
+                    $("#signOut").css("display", "block");
+                }
+                //admin
+                if (permission == 1){
+                    $("#adminNav").css("display", "block");
+                    $("#careService").css("display", "block");
+                    $("#volunteerNav").css("display", "block");
+                }
+                
+                //youth
+                if (permission == 2){
+                    $("#youthNav").css("display", "block");
+                    $("#personalAreaLink").css("display", "block");
+                }
+                
+                //shelter
+                if (permission == 3){
+                    $("#careService").css("display", "block");
+                    $("#youthNav").css("display", "none");
+                }
+                
+                //support group
+                if (permission == 4){
+                    $("#careService").css("display", "block");
+                    $("#youthNav").css("display", "none");
+                }
+                
+                //volunteer
+                if (permission == 5){
+                    $("#volunteerNav").css("display", "block");
+                    $("#youthNav").css("display", "none");
+                }
+                
+        </script>
+        <hr>
         <main>
+            <div class="container">
             <div class="form">
                 <h2 id="pageTitle">
                     הצטרפות בית מחסה
@@ -121,9 +275,8 @@
                         <label for="phone">
                             טלפון בית המחסה:
                         </label>
-                        <input type="tel" class="form-control" id="phone" name="phone" placeholder="הכנס/י את מספר הטלפון של בית המחסה" required title="אנא הכנס/י מספר טלפון המכיל 9-10 ספרות" pattern="(?=.*[0-9]).{9-11}"
+                        <input type="tel" class="form-control" id="phone" name="phone" placeholder="הכנס/י את מספר הטלפון של בית המחסה" required pattern="^[0-9]{9,11}$" title="אנא הכנס/י מספר טלפון המכיל 9-11 ספרות"
                         oninvalid="setCustomValidity(' אנא הכנס/י מספר טלפון המכיל 9-11 ספרות ')"
-                         onchange="this.setCustomValidity(this.validity.patternMismatch ? this.title : '');"
                        />
                     </div>
                     <div class="form-group">
@@ -232,13 +385,14 @@
                     </div>
                 </form>
             </div>
+            </div>
         </main>
         
         <footer class="footer">
             <div class="container">
                 <div class="row">
                     <div class="col-sm-3">
-                        <a href="#"><img src="logo_icon.png" id="logo_foo"></a>
+                        <a href="#"><img id="logo_foo" src="logo_icon.png" ></a>
                     </div>
                     <div class="col-sm-9">
                         <h4 class="title" >הושט לי יד - ארגון לנוער במצבי סיכון</h4>
@@ -259,7 +413,37 @@
                 </div>
             </div>	
         </footer>
+        <script>
+        $(document).ready(function () {
+  var trigger = $('.hamburger'),
+      overlay = $('.overlay'),
+     isClosed = false;
+
+    trigger.click(function () {
+      hamburger_cross();      
+    });
+
+    function hamburger_cross() {
+
+      if (isClosed == true) {          
+        overlay.hide();
+        trigger.removeClass('is-open');
+        trigger.addClass('is-closed');
+        isClosed = false;
+      } else {   
+        overlay.show();
+        trigger.removeClass('is-closed');
+        trigger.addClass('is-open');
+        isClosed = true;
+      }
+  }
+  
+  $('[data-toggle="offcanvas"]').click(function () {
+        $('#wrapper').toggleClass('toggled');
+  });  
+});
         
+</script>
          
             
         <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js">
@@ -323,6 +507,7 @@
           elements[0].oninput = function(e) {
              e.target.setCustomValidity("");
                 };
+            });
            
         </script>
         
@@ -346,7 +531,40 @@
         
         <script>
             function homePage(){
-                location.href='http://gabriellasi.mtacloud.co.il/home%20page/homePage.html';
+                location.href='http://gabriellasi.mtacloud.co.il/home%20page/homePage.php';
+            }
+        </script>
+          <script>
+        $('#phone').keyup(validatePhone);
+
+        function validatePhone() {
+            var errorMsg = "אנא הכנס/י מספר טלפון המכיל 9-11 ספרות";
+            var phone = this;
+            var pattern = new RegExp('^' + $(phone).attr('pattern') + '$');
+            // check each line of text
+            $.each($(this).val().split("\n"), function () {
+                // check if the line matches the pattern
+                var hasError = !this.match(pattern);
+                if (typeof phone.setCustomValidity === 'function') {
+                    phone.setCustomValidity(hasError ? errorMsg : '');
+                } else {
+                    // Not supported by the browser, fallback to manual error display...
+                    $(phone).toggleClass('error', !!hasError);
+                    $(phone).toggleClass('ok', !hasError);
+                    if (hasError) {
+                        $(phone).attr('title', errorMsg);
+                    } else {
+                        $(phone).removeAttr('title');
+                    }
+                }
+                return !hasError;
+            });
+        }
+        </script>
+                <script>
+            function refreshPage(){
+                window.location.reload();
+                location.href='#';
             }
         </script>
  <script>
